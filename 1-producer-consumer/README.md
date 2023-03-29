17b67db6a622a9630325c0a935902154744224bc
jiangxi04 <jiangxi04@baidu.com>
2023-03-29 17:01:54 +0800

    finish 1

--- 1-producer-consumer/README.md
+++ 1-producer-consumer/README.md
@@ -1,7 +1,7 @@
 # Producer-Consumer Scenario
 
-The producer reads in tweets from a mockstream and a consumer is processing the data to find out whether someone has tweeted about golang or not. The task is to modify the code inside `main.go` so that producer and consumer can run concurrently to increase the throughput of this program.
-
+The producer reads in tweets from a mockstream and a consumer is processing the data to find out whether someone has tweeted about golang or not. The task is to modify the code inside `main.go` so that producer and consumer can run concurrently to increase the throughput of this program.  
+生产者从一个模拟数据流中读取推文，消费者则处理这些数据以找出是否有人发了有关 Golang 的推文。任务是修改 main.go 中的代码，以便生产者和消费者可以并发运行，从而提高程序的吞吐量。
 ## Expected results:
 Before: 
 ```
--- 1-producer-consumer/main.go
+++ 1-producer-consumer/main.go
@@ -10,28 +10,32 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func producer(stream Stream) (tweets []*Tweet) {
+func producer(stream Stream, tweets chan *Tweet) {
 	for {
 		tweet, err := stream.Next()
 		if err == ErrEOF {
-			return tweets
+			close(tweets)
+			return
 		}
 
-		tweets = append(tweets, tweet)
+		//tweets = append(tweets, tweet)
+		tweets <- tweet
 	}
 }
 
-func consumer(tweets []*Tweet) {
-	for _, t := range tweets {
+func consumer(tweets chan *Tweet) {
+	for t := range tweets {
 		if t.IsTalkingAboutGo() {
 			fmt.Println(t.Username, "\ttweets about golang")
 		} else {
 			fmt.Println(t.Username, "\tdoes not tweet about golang")
 		}
 	}
+	return
 }
 
 func main() {
@@ -39,10 +43,19 @@ func main() {
 	stream := GetMockStream()
 
 	// Producer
-	tweets := producer(stream)
-
+	//tweets := producer(stream)
+	tweets := make(chan *Tweet) //有无缓冲在这里使用上区别不大
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		producer(stream, tweets)
+		wg.Done()
+	}()
 	// Consumer
-	consumer(tweets)
-
+	go func() {
+		consumer(tweets)
+		wg.Done()
+	}()
+	wg.Wait()
 	fmt.Printf("Process took %s\n", time.Since(start))
 }
